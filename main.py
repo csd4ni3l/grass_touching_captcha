104b87d5559c85097bdddcb269abe6b1a58eb0e8
csd4ni3l <v3xq21w7@duck.com>
2025-08-09 11:39:11 +0200

    fix png not working because of removing too many bytes of data url header

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ login_manager.init_app(app)
 
 os.makedirs("uploads", exist_ok=True)
 
-users = {}
-
 class User(flask_login.UserMixin):
     pass
 
@@ -57,9 +55,12 @@ def resize_image_file(path, max_side=256, fmt="JPEG"):
 @app.route("/upload", methods=["POST"])
 def upload():
     try:
-        image_type, image_data = request.json["image_type"], request.json["image_data"].encode("utf-8")
+        image_type, image_data = request.json["username"], request.json["image_type"], request.json["image_data"].encode("utf-8")
 
-        image_data = image_data[23:] # data:image/jpeg;base64,
+        if image_type == "jpeg":
+            image_data = image_data[23:] # data:image/jpeg;base64,
+        else:
+            image_data = image_data[22:]
 
         image_uuid = uuid.uuid4()
 
