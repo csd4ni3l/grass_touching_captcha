638193fcda8fdfd9c0ce8c7bde5d61d75ee751e5
csd4ni3l <v3xq21w7@duck.com>
2025-08-09 12:46:57 +0200

    Fix username not gathered in the upload route

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def resize_image_file(path, max_side=256, fmt="JPEG"):
 @app.route("/upload", methods=["POST"])
 def upload():
     try:
-        username, image_type, image_data = request.json["image_type"], request.json["image_data"].encode("utf-8")
+        username, image_type, image_data = request.json["username"], request.json["image_type"], request.json["image_data"].encode("utf-8")
 
         if image_type == "jpeg":
             image_data = image_data[23:] # data:image/jpeg;base64,
